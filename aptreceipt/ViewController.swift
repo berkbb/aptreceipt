bc77615b9e2b0292694ff31a0a3902153532f4f7
berkbb <berkbaba_dev@outlook.com>
2022-03-16 18:24:23 +0300

    Click functions added.

--- aptreceipt/ViewController.swift
+++ aptreceipt/ViewController.swift
@@ -9,6 +9,14 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+ 
+    @IBOutlet weak var HomeHumber_Selector: NSPopUpButton!
+    @IBOutlet weak var DatePicker_Receipt: NSDatePicker!
+    @IBOutlet weak var SupplierName_textControl: NSTextField!
+    @IBOutlet weak var OwnerName_textControl: NSTextField!
+    @IBOutlet weak var SeqNumber_textControl: NSTextField!
+    @IBOutlet weak var Total_TextControl: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +29,18 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func Clear_Click(_ sender: NSButton) {
+        HomeHumber_Selector.selectItem(at: 0)
+        DatePicker_Receipt.dateValue=Date()
+        Total_TextControl.stringValue=""
+        SeqNumber_textControl.stringValue=""
+        SupplierName_textControl.stringValue="Mustafa Cem Babadoğan"
+        OwnerName_textControl.stringValue=""
+     
+        
+    }
+    
+    @IBAction func Create_Click(_ sender: NSButton) {
+    }
 }
 
