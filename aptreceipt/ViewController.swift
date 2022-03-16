a45becc85845506414942662d27510f3cde55df0
berkbb <berkbaba_dev@outlook.com>
2022-03-16 14:42:42 +0300

    Initial commit

---
+++ aptreceipt/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  aptreceipt
+//
+//  Created by Berk Babadoğan on 16.03.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
