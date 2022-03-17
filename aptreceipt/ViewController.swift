898d59b9359548d2ccd7aeaa55c87fe60a109118
berkbb <berkbaba_dev@outlook.com>
2022-03-17 17:06:48 +0300

    Create button gathers data now.
    After gathering, print specialized view as as image please.

--- aptreceipt/ViewController.swift
+++ aptreceipt/ViewController.swift
@@ -16,9 +16,12 @@ class ViewController: NSViewController {
     @IBOutlet weak var OwnerName_textControl: NSTextField!
     @IBOutlet weak var SeqNumber_textControl: NSTextField!
     @IBOutlet weak var Total_TextControl: NSTextField!
+    @IBOutlet weak var Info_TextControl: NSTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
 
         // Do any additional setup after loading the view.
     }
@@ -28,7 +31,7 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
+    /// Clear button click  event.
     @IBAction func Clear_Click(_ sender: NSButton) {
         HomeHumber_Selector.selectItem(at: 0)
         DatePicker_Receipt.dateValue=Date()
@@ -36,11 +39,78 @@ class ViewController: NSViewController {
         SeqNumber_textControl.stringValue=""
         SupplierName_textControl.stringValue="Mustafa Cem Babadoğan"
         OwnerName_textControl.stringValue=""
+        Info_TextControl.stringValue="";
+        
      
         
     }
-    
+   
+    /// Create button click  event.
     @IBAction func Create_Click(_ sender: NSButton) {
+        
+        
+        
+        let selectedApartment=HomeHumber_Selector.selectedItem?.title
+        print("Apartment number: \(selectedApartment!)")
+        let issueDate=getDatewithMonthDate(sentDate: DatePicker_Receipt.dateValue)
+        if !issueDate.isEmpty {
+            print("Issue Date: \(issueDate)")
+        }
+        let seqNumber=SeqNumber_textControl.stringValue
+        if !seqNumber.isEmpty {
+            print("Sequence Number \(seqNumber)")
+        }
+    
+      
+        let supplierName=SupplierName_textControl.stringValue.localizedCapitalized
+        if !supplierName.isEmpty {
+            print("Supplier Name: \(supplierName)")
+        }
+       
+        let ownerName=OwnerName_textControl.stringValue.localizedCapitalized
+        if !ownerName.isEmpty {
+            print("Owner Name: \(ownerName)")
+        }
+        let total=Total_TextControl.stringValue
+         if !total.isEmpty {
+             print("Total: \(total)")
+         }
+       
+     
+        var info=Info_TextControl.stringValue;
+        if !info.isEmpty {
+            print("Info: \(info)")
+        }
+        else
+        {
+            info=" "
+        }
+        
+        if( !selectedApartment!.isEmpty  && !seqNumber.isEmpty && !total.isEmpty && !supplierName.isEmpty && !ownerName.isEmpty && !issueDate.isEmpty )
+        {
+            print("OK")
+            
+        }
+       
+    }
+    
+    /// Get month and year from Date from given 'sentDate'
+    ///
+    /// - Warning: The returned string is  localized.
+    /// - Parameter subject: The subject  is Date object.
+    /// - Returns: A date string to the `sentDate` like XXXX YYYY.
+    func getDatewithMonthDate(sentDate:Date) -> String
+    {
+        let dateFormatter1 = DateFormatter()
+        dateFormatter1.dateFormat = "yyyy"
+        let yearString = dateFormatter1.string(from: sentDate)
+        let dateFormatter2 = DateFormatter()
+        dateFormatter2.dateFormat = "LLLL"
+        let monthString = dateFormatter2.string(from: sentDate)
+        let returnValue = "\(monthString) \(yearString)"
+        
+      
+        return returnValue
     }
 }
 
