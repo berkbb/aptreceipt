b3e8193885847dc441063d226ea7f501adaf2639
berkbb <berkbaba_dev@outlook.com>
2022-03-19 18:06:24 +0300

    Print completed.

--- aptreceipt/ReceiptViewController.swift
+++ aptreceipt/ReceiptViewController.swift
@@ -18,7 +18,8 @@ class ReceiptViewController: NSViewController {
     var seqNumber:Int=0
     var currentDate:String=""
     
-
+    @IBOutlet weak var ReceiptView: NSView!
+    
     @IBOutlet weak var Receipt_IssuerLabel: NSTextField!
     @IBOutlet weak var Total_MesaageLabel: NSTextField!
     @IBOutlet weak var Notes_Print: NSTextField!
@@ -28,6 +29,7 @@ class ReceiptViewController: NSViewController {
     @IBOutlet weak var Price_Label: NSTextField!
     @IBOutlet weak var Seq_Label: NSTextField!
     @IBOutlet weak var CurrentDate_Label: NSTextField!
+    @IBOutlet weak var Receipt: NSBox!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
@@ -50,12 +52,36 @@ class ReceiptViewController: NSViewController {
         Seq_Label.stringValue=String(seqNumber)
         CurrentDate_Label.stringValue=currentDate
     }
+    /// Cancel click function
     @IBAction func Cancel_Click(_ sender: Any) {
         
         if let controller=self.storyboard?.instantiateController(withIdentifier: "HomeView") as? ViewController{self.view.window?.contentViewController=controller}
     }
     
+    /// Save click function
     @IBAction func Save_Click(_ sender: Any) {
+        
+        let image=Receipt.asImage();
+   
+        let savePanel = NSSavePanel()
+            savePanel.canCreateDirectories = true
+            savePanel.showsTagField = false
+        savePanel.nameFieldStringValue = "\(recevier.lowercased())_\(monthYear.lowercased()).jpg"
+        savePanel.allowedFileTypes = ["jpg"]
+        savePanel.title=localizedString(forKey: "saveImagetitle")
+        if (savePanel.runModal() == NSApplication.ModalResponse.OK) {
+                    let result = savePanel.url
+
+                   if (result != nil) {
+
+                       image.writeJPEG(toURL: result!)
+                       print("saved at \(result!)")
+                   }
+               } else {
+                   print("Cancel")
+                   return // User clicked cancel
+               }
+    
     }
     
     
@@ -75,4 +101,41 @@ class ReceiptViewController: NSViewController {
 
         return result
     }
+    
+
 }
+extension NSBox {
+
+    // Using a function since `var image` might conflict with an existing variable
+    // (like on `UIImageView`)
+    func asImage() -> NSImage {
+        let viewToCapture = self
+        let rep = viewToCapture.bitmapImageRepForCachingDisplay(in: viewToCapture.bounds)!
+        viewToCapture.cacheDisplay(in: viewToCapture.bounds, to: rep)
+
+        let img = NSImage(size: viewToCapture.bounds.size)
+        img.addRepresentation(rep)
+        return img
+        }
+    
+    }
+
+extension NSImage {
+   public func writeJPEG(toURL url: URL) {
+
+       guard let data = tiffRepresentation,
+           let rep = NSBitmapImageRep(data: data),
+             let imgData = rep.representation(using: .jpeg, properties: [.compressionFactor : NSNumber(floatLiteral: 1.0)]) else {
+
+               print("\(self.self) Error Function '\(#function)' Line: \(#line) No tiff rep found for image writing to \(url)")
+               return
+       }
+
+       do {
+           try imgData.write(to: url)
+       }catch let error {
+           print("\(self.self) Error Function '\(#function)' Line: \(#line) \(error.localizedDescription)")
+       }
+   }
+}
+
